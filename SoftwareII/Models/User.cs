585b9c76c8b320cc5a0c8fcdcf5568f99c434abd
Kevin Andrés Vanegas Medina <vanegasmk@gmail.com>
2021-04-06 15:27:51 -0600

    Update Models

--- SoftwareII/Models/User.cs
+++ SoftwareII/Models/User.cs
@@ -6,18 +6,14 @@ namespace SoftwareII.Models
     public class User
     {
         public long Id { get; set; }
-
         public string DNI { get; set; }
         public string Name { get; set; }
         public string FirstSurname { get; set; }
         public string LastSurname { get; set; }
-        public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
-        public string WorkAddress { get; set; }
-        public string Image { get; set; }
 
 
     }
